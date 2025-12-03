77e575f7f15c287e606c47e0a1df6873fd828d22
David Guaman <david.a.guaman@unl.edu.ec>
2025-12-03 07:56:55 -0500

    actualizacion

--- SimulacionCarro/control.py
+++
@@ -1,5 +0,0 @@
-# control.py
-def detener_vehiculo(sim: "Simulacion", id_objetivo):
-    for v in sim.vehiculos:
-        if v.id == id_objetivo:
-            v.detener()
--- SimulacionCarro/utilidades.py
+++
@@ -1,5 +0,0 @@
-# utilidades.py
-import math
-
-def distancia_angular(a1: float, a2: float) -> float:
-    return (a2 - a1) % (2 * math.pi)
